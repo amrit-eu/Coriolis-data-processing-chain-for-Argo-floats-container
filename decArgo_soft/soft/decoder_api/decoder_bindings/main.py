8cf59c3bc4d71dff34d988451d1447283da4114b
jamclar <jamclar@noc.ac.uk>
2025-09-25 10:05:21 +0100

    Tweaks

--- decArgo_soft/soft/decoder_api/decoder_bindings/main.py
+++ decArgo_soft/soft/decoder_api/decoder_bindings/main.py
@@ -18,12 +18,15 @@ class ExecutionError(Exception):
 class DecoderConfiguration(BaseModel):
     """Configuration used to pass to the decoder, with validation applied."""
 
-    input_files_directory: Path
-    output_files_directory: Path
+    input_files_directory: Path | None
+    output_files_directory: Path | None
+    decoder_conf_file: Path
 
     @field_validator("input_files_directory", mode="before")
     def check_input_files_directory(cls, input_directory: Path):
-        """Validate then resolve the input files directory."""
+        """Validate then resolve the input files directory if not None."""
+        if input_directory is None:
+            return input_directory
         if not input_directory.is_dir():
             raise ValueError(f"{input_directory} is not a valid input directory!")
 
@@ -33,7 +36,9 @@ class DecoderConfiguration(BaseModel):
 
     @field_validator("output_files_directory", mode="before")
     def check_output_files_directory(cls, output_directory: Path):
-        """Validate then resolve the output files directory."""
+        """Validate then resolve the output files directory if not None"""
+        if output_directory is None:
+            return output_directory
         if not output_directory.is_dir():
             raise ValueError(f"{output_directory} is not a valid output directory!")
         return output_directory.resolve()
@@ -42,10 +47,12 @@ class DecoderConfiguration(BaseModel):
 class Decoder:
     """Decoder Bindings."""
 
-    def __init__(self, input_files_directory: str, output_files_directory: str):
+    def __init__(self, input_files_directory: str | None, output_files_directory: str | None, decoder_conf_file: str):
         """Initialise the bindings instance."""
         self.config = DecoderConfiguration(
-            input_files_directory=Path(input_files_directory), output_files_directory=Path(output_files_directory)
+            input_files_directory=Path(input_files_directory) if isinstance(input_files_directory, str) else None,
+            output_files_directory=Path(output_files_directory) if isinstance(output_files_directory, str) else None,
+            decoder_conf_file=Path(decoder_conf_file)
         )
 
     def decode(self, wmonum: str) -> None:
@@ -55,18 +62,22 @@ class Decoder:
             "rsynclog",
             "all",
             "configfile",
-            "/mnt/data/config/decoder_conf.json",
+            str(self.config.decoder_conf_file),
             "xmlreport",
             "logfilexml.xml",
             "floatwmo",
             wmonum,
             "PROCESS_REMAINING_BUFFERS",
-            "1",
-            "DIR_INPUT_RSYNC_DATA",
+            "1"
+        ]
+        # IF passed, extend the command to include the new input/output arguments to the decoder.
+        if self.config.input_files_directory is not None:
+            cmd.extend(
+            ["DIR_INPUT_RSYNC_DATA",
             str(self.config.input_files_directory),
             "DIR_OUTPUT_NETCDF_FILE",
-            str(self.config.output_files_directory),
-        ]
+            str(self.config.output_files_directory)])
+
         # Regarding the 'except' clauses, these will return various non 200 status codes when integrated into the API.
         try:
             result = subprocess.run(cmd, env=os.environ.copy(), check=True)
@@ -77,6 +88,8 @@ class Decoder:
             print("Invalid command")
         else:
             print("Decoding ran:", result)
+        while True:
+            pass
 
 
 if __name__ == "__main__":  # pragma: no cover
@@ -88,5 +101,7 @@ if __name__ == "__main__":  # pragma: no cover
     #     raise ExecutionError("Usage: main.py <WMONUM>")
 
     # These are hardcoded for now, but will likely be passed by the calling code.
-    decoder = Decoder("/mnt/data/rsync/archive/cycle", "/mnt/data/output/nc/")
+    decoder = Decoder(input_files_directory=None,
+                      output_files_directory=None,
+                      decoder_conf_file="/home/airflow/decoder_project/config_files/decoder_conf.json")
     decoder.decode("6902892")
